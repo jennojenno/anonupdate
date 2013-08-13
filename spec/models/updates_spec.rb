d876ddcb111016f1e9da1860210abc92d60f1f8a
Jen Lim <jennolim@gmail.com>
2013-08-13 16:21:02 -0400

    Rspec cleaner User test and Update model tests

--- spec/models/updates_spec.rb
+++ spec/models/updates_spec.rb
@@ -2,21 +2,23 @@ require 'spec_helper'
 
 describe Update do
   before do 
-    @update = Update.create
-    @update.user_id = 1
-    @update.save!
+    @user = User.create(:name => 'Jon')
+    @myupdate = Update.create
+    @myupdate.myupdate = "Hello world"
+    @myupdate.user_id = @user.id
   end 
 
   it 'exists' do
-    expect(@update).to_not be_nil
+    expect(@myupdate).to_not be_nil
   end
 
   context 'associations' do 
     it {should belong_to(:user)}
   end 
  
-  it 'belongs to user 1' do 
-    expect (@update.user_id).to eq(1)
-    puts subject
-  end
 end
+
+
+#Things to test for Update models... 
+- Can we get the correct update as user Jon? 
+- 
--- spec/models/users_spec.rb
+++ spec/models/users_spec.rb
@@ -1,35 +1,43 @@
 require 'spec_helper'
 
 describe User do
+  describe "Jon" do 
 
-  before do 
-    @user = User.create(:name => 'Jon')
-    @myupdate = Update.new
-    @myupdate.myupdate = "Hello world"
-    @myupdate.user_id = @user.id
-  end 
+    before do 
+      @user = User.create(:name => 'Jon')
+      @myupdate = Update.create
+      @myupdate.myupdate = "Hello world"
+      @myupdate.user_id = @user.id
+    end 
 
-  it 'exists' do
-    expect(@user).to_not be_nil
-  end
+    it 'exists' do
+      expect(@user).to_not be_nil
+    end
 
-  context 'associations' do 
-    it {should have_many(:friends)}
-    it {should have_many(:updates)}
-  end 
+    context 'associations' do 
+      it {should have_many(:friends)}
+      it {should have_many(:updates)}
+    end 
 
-  # it 'can create an update' do 
-  #   @myupdate = Update.new
-  #   @myupdate.myupdate = "Hello world"
-  #   @myupdate.user_id = @user.id
-  # end    
+    # it 'can create an update' do 
+    #   @myupdate = Update.new
+    #   @myupdate.myupdate = "Hello world"
+    #   @myupdate.user_id = @user.id
+    # end    
 
-  it 'should have an update associated with it' do 
-    updatecount = Update.where("user_id" => @myupdate.id)
-    updatecount.count.should == 1
-  end 
+    it 'should have an update associated with it' do 
+      @updatecount = Update.where("user_id" => @user.id)
+      @updatecount.count.should == 1
+    end 
 
-  it 'should know what class it is' do 
-    @user.class.should == User
+    it 'should know what class it is' do 
+      @user.class.should == User
+    end 
+
   end 
 end
+
+#Things to test in our User models
+# - Can I have 3 friends?
+# - Can 2/3 friends have updates?
+# - Can I see all my friends' updates? 
